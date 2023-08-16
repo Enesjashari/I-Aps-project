047645bdea1b7141cda4458c784773ed380af42c
Enes <enesjashari2004@gmail.com>
2023-08-16 15:45:51 +0200

    Update 1

--- project/app/models.py
+++ project/app/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+# import uuid
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
 
 class Paper(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    id = models.CharField(max_length=100,primary_key=True)
     title = models.CharField(max_length=200)
     abstract = models.TextField()
     authors = models.ManyToManyField(Author)
--- project/app/urls.py
+++ project/app/urls.py
@@ -3,7 +3,7 @@ from .views import PaperList, PaperDetail, AuthorList, AuthorDetail, CategoryLis
 
 urlpatterns = [
     path('papers/', PaperList.as_view(), name='paper-list'),
-    path('papers/<int:pk>/', PaperDetail.as_view(), name='paper-detail'),
+    path('papers/<str:pk>/', PaperDetail.as_view(), name='paper-detail'),
     path('authors/', AuthorList.as_view(), name='author-list'),
     path('authors/<int:pk>/', AuthorDetail.as_view(), name='author-detail'),
     path('categories/', CategoryList.as_view(), name='category-list'),
